c6ad1817db907a9390d41c3e7c88b4ded8d30ca0
Arsen <arsenguzhva@gmail.com>
2017-04-27 12:00:43 +0300

    add products and stuff

---
+++ src/main/java/club/psvm/smallshopsnetwork/domain/RawStuff.java
@@ -0,0 +1,41 @@
+package club.psvm.smallshopsnetwork.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by JAVA-P on 27.04.2017.
+ */
+ 
+ /* Создано в www.gateon.net
+ * Все права на данный программный код принадлежат компании GateOn
+ * Created by www.gateon.net
+ * All rights to the software code are owned by GateOn
+*/
+
+ @Entity
+public class RawStuff {
+
+     @Id
+     @GeneratedValue
+     Long id;
+
+     String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
