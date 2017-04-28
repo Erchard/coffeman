445e36336b5459d2d9ad7972522088f0720f15d0
Arsen <arsenguzhva@gmail.com>
2017-04-28 16:48:23 +0300

    init service

--- src/main/java/club/psvm/smallshopsnetwork/test/TestOne.java
+++ src/main/java/club/psvm/smallshopsnetwork/test/TestOne.java
@@ -4,4 +4,9 @@ package club.psvm.smallshopsnetwork.test;
  * Created by andy on 28.04.17.
  */
 public class TestOne {
+
+
+    public TestOne() {
+        System.out.println("Отлично! Я смержил твой коммит");
+    }
 }
